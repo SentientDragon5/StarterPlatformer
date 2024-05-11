a0466a6ef3d22405eaf8667a185eaa721fb14493
SentientDragon5 <sentientdragon5@gmail.com>
2024-05-11 15:43:53 -0700

    Update Load.js

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -9,6 +9,8 @@ class Load extends Phaser.Scene {
         // Load characters spritesheet
         this.load.atlas("platformer_characters", "tilemap-characters-packed.png", "tilemap-characters-packed.json");
 
+        this.load.atlas("meow-knight", ".png", "tilemap-characters-packed.json");
+
         // Load tilemap information
         this.load.image("tilemap_tiles", "tilemap_packed.png");                         // Packed tilemap
         this.load.tilemapTiledJSON("platformer-level-1", "platformer-level-1.tmj");   // Tilemap in JSON
@@ -47,6 +49,37 @@ class Load extends Phaser.Scene {
                 { frame: "tile_0001.png" }
             ],
         });
+
+        // ===== OLD alien =====
+        // this.anims.create({
+        //     key: 'walk',
+        //     frames: this.anims.generateFrameNames('platformer_characters', {
+        //         prefix: "tile_",
+        //         start: 0,
+        //         end: 1,
+        //         suffix: ".png",
+        //         zeroPad: 4
+        //     }),
+        //     frameRate: 15,
+        //     repeat: -1
+        // });
+
+        // this.anims.create({
+        //     key: 'idle',
+        //     defaultTextureKey: "platformer_characters",
+        //     frames: [
+        //         { frame: "tile_0000.png" }
+        //     ],
+        //     repeat: -1
+        // });
+
+        // this.anims.create({
+        //     key: 'jump',
+        //     defaultTextureKey: "platformer_characters",
+        //     frames: [
+        //         { frame: "tile_0001.png" }
+        //     ],
+        // });
 
          // ...and pass to the next Scene
          this.scene.start("platformerScene");
